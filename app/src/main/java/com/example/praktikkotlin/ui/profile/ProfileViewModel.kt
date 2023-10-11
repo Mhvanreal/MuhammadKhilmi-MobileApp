cda4ceee3cd903f0b590a5c58f33b74cbdb3766e
mhvanreal <hilmyofficial03@gmail.com>
2023-10-12 01:15:54 +0700

    revisi name

--- app/src/main/java/com/example/praktikkotlin/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/praktikkotlin/ui/profile/ProfileViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Hasan Nugrahadi"
+        value = "Muhammad Khilmi"
     }
     val text: LiveData<String> = _text
 
